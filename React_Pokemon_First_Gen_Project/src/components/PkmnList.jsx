9721b0bd683abed05d8e9967cec991ff944bc6b3
hobisognodiunarma <alessandroarmari22@gmail.com>
2023-01-23 14:13:29 +0100

    need to fix issues

--- React_Pokemon_First_Gen_Project/src/components/PkmnList.jsx
+++ React_Pokemon_First_Gen_Project/src/components/PkmnList.jsx
@@ -3,26 +3,44 @@ import { Card } from "./Card";
 
 const PkmnList = (props) => {
   const [teamDisplay, setTeamDisplay] = useState(false);
+  const [firstPkmn, setFirstPkmn] = useState("");
 
+  const addPkmnOnClickFunction = (event) => {
+    setTeamDisplay(true);
+    console.log("You chose " + event.target.name.value); //sss
+    setFirstPkmn(event.target.sprite);
+  };
   return (
-    <ul className="container list-unstyled">
-      <div className="row">
-        {props.list.pokedex.map((elem, index) => {
-          return (
-            <Card
-              key={index}
-              addPkmnOnClick={() => {
-                setTeamDisplay(true);
-              }} //WHEN I CLICK AN IMAGE!
-              sprite={elem.sprite}
-              name={elem.name}
-              number={elem.number}
-              type={elem.type}
+    <>
+      {teamDisplay && (
+        <section className="m-4 d-flex flex-column align-items-center border border-3 border-danger rounded ">
+          <h3>Your Team</h3>
+          <div>
+            <img
+              src={firstPkmn}
+              alt="firstPkmn"
+              className="mb-1 border border-2 border-primary rounded"
             />
-          );
-        })}
-      </div>
-    </ul>
+          </div>
+        </section>
+      )}
+      <ul className="container list-unstyled">
+        <div className="row">
+          {props.list.pokedex.map((elem, index) => {
+            return (
+              <Card
+                key={index}
+                addPkmnOnClick={addPkmnOnClickFunction}
+                sprite={elem.sprite}
+                name={elem.name}
+                number={elem.number}
+                type={elem.type}
+              ></Card>
+            );
+          })}
+        </div>
+      </ul>
+    </>
   );
 };
 
